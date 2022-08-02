6ad05c908a53d6dc4f45df7768c56afeec1c530f
vasilikapapa <vasilikapp@gmail.com>
2022-08-01 20:06:48 -0500

    Angular-studio-part1-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    XyzComponent,
+    HeaderComponent,
+    CrewComponent,
+    ExperimentsComponent,
+    EquipmentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
